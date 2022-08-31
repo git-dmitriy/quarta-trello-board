3900747b3cf8637b62aa01c13ed3660123926910
git-dmitriy <shalberkind@mail.ru>
2022-08-31 23:26:55 +0300

    Create dark mode toggler

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 import { navbar } from './navbar';
+import { toggleDarkMode } from './darkmode';
 
 const btn = document.querySelector('.floating-btn');
 const backdrop = document.querySelector('.backdrop');
 const nav = document.querySelector('.settings-wrapper');
+const nightModeSwitcher = document.querySelector('[data-night-mode-switcher]');
+
 navbar({ btn, backdrop, nav, backDropActiveClass: 'backdrop_show' });
+
+toggleDarkMode({ btn: nightModeSwitcher, className: 'dark' });
+
