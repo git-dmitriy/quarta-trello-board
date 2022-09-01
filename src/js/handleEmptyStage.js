3fb49abc6e76c679bc6cb5975b20ae1f9034621d
git-dmitriy <shalberkind@mail.ru>
2022-09-01 08:23:09 +0300

    Refactoring

--- src/js/handleEmptyStage.js
+++ src/js/handleEmptyStage.js
@@ -1,21 +1,22 @@
 export const handleEmptyStage = (selector) => {
   const stages = document.querySelectorAll(selector);
-  const observer = new MutationObserver((record) => {
-    const element = record[0].target;
-    if (element.childElementCount === 0) {
-      element.setAttribute('data-empty', 'data-empty');
-    }
-    if (element.childElementCount > 0) {
-      element.removeAttribute('data-empty');
-    }
+
+  const observer = new MutationObserver(() => {
+    stages.forEach((element) => {
+      setDataEmptyAttr(element);
+    });
   });
 
   stages.forEach((element) => {
     observer.observe(element, { childList: true });
-    if (element.children.length === 0) {
-      element.setAttribute('data-empty', 'data-empty');
-    } else {
-      element.removeAttribute('data-empty');
-    }
+    setDataEmptyAttr(element);
   });
+};
+
+const setDataEmptyAttr = (element) => {
+  if (element.children.length === 0) {
+    element.setAttribute('data-empty', 'data-empty');
+  } else {
+    element.removeAttribute('data-empty');
+  }
 };
