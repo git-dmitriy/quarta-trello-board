2562b062f9f05146635bb6641c335564489c6ee4
git-dmitriy <shalberkind@mail.ru>
2022-09-01 08:18:46 +0300

    Make tasks draggable

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import { navbar } from './navbar';
 import { toggleDarkMode } from './darkmode';
 import { handleEmptyStage } from './handleEmptyStage';
+import { draggableTasks } from './draggable-tasks';
 
 const btn = document.querySelector('.floating-btn');
 const backdrop = document.querySelector('.backdrop');
@@ -12,3 +13,5 @@ navbar({ btn, backdrop, nav, backDropActiveClass: 'backdrop_show' });
 toggleDarkMode({ btn: nightModeSwitcher, className: 'dark' });
 
 handleEmptyStage('.task-list');
+
+draggableTasks({ elSelector: '.task-item', dropzoneSelector: '.task-list' });
